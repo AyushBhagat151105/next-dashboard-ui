f016de723bb3544e8b95430583f53d033bce3b28
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-12-04 16:00:39 +0530

    add stand on teacher page in list

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import moment from "moment";
 import { calendarEvents } from "@/lib/data";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
 
 const localizer = momentLocalizer(moment);
@@ -14,18 +16,18 @@ const BigCalendar = () => {
   };
 
   return (
-    <Calendar
-      localizer={localizer}
-      events={calendarEvents}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: "98%" }}
-      views={["work_week", "day"]}
-      view={view}
-      onView={handleOnChangeView}
-      min={new Date(2025, 1, 0, 8, 0, 0)}
-      max={new Date(2025, 1, 0, 17, 0, 0)}
-    />
+      <Calendar
+          localizer={localizer}
+          events={calendarEvents}
+          startAccessor="start"
+          endAccessor="end"
+          views={["work_week", "day"]}
+          view={view}
+          style={{ height: "98%" }}
+          onView={handleOnChangeView}
+          min={new Date(2025, 1, 0, 8, 0, 0)}
+          max={new Date(2025, 1, 0, 17, 0, 0)}
+      />
   );
 };
 
